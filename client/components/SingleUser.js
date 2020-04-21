130988c60e0656220a8e8d87f0f3767249893378
Eddie Haddican <edwardhaddican@gmail.com>
2020-04-21 17:34:39 -0400

    fixed issue with auto logging in when a single user was selected

--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {fetchUser} from '../redux/user.js'
-// import {Navbar} from './components'
-// import Routes from './routes'
+import {fetchUser} from '../redux/singleUser.js'
 
 export class SingleUser extends React.Component {
   async componentDidMount() {
     if (this.props.fetchUser) {
       try {
-        console.log('>>>>', this.props.match.params.id)
         await this.props.fetchUser(this.props.match.params.id)
       } catch (err) {
         console.log(err)
@@ -18,10 +15,10 @@ export class SingleUser extends React.Component {
   }
 
   render() {
-    return this.props.user.id ? (
-      <div className="single-user" key={this.props.user.id}>
-        <h1>{this.props.user.email}</h1>
-        <img src={this.props.user.imageUrl} alt="" />
+    return this.props.singleUser.id ? (
+      <div className="single-user" key={this.props.singleUser.id}>
+        <h1>{this.props.singleUser.email}</h1>
+        <img src={this.props.singleUser.imageUrl} alt="" />
       </div>
     ) : (
       <h3>No User</h3>
@@ -31,7 +28,7 @@ export class SingleUser extends React.Component {
 
 const mapState = state => {
   return {
-    user: state.user
+    singleUser: state.singleUser
   }
 }
 
--- client/redux/index.js
+++ client/redux/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from 'redux'
 import allUsersReducer from './allUsers'
-import userReducer from './user'
+import userReducer from '../store/user'
+import singleUserReducer from './singleUser'
 
 const appReducer = combineReducers({
   users: allUsersReducer,
-  user: userReducer
+  user: userReducer,
+  singleUser: singleUserReducer
 })
 
 export default appReducer
