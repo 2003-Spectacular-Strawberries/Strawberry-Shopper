39c84d766387012023c0468324d06a45228775af
Eddie Haddican <edwardhaddican@gmail.com>
2020-04-21 11:50:22 -0400

    all products and single product pages work, this need to be connected to the db once it is made and seeded

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -9,20 +9,26 @@ const testArrayOfObjects = [
     name: 'strawberryShortCake',
     price: 10,
     image:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thermofisher.com%2Fblog%2Ffood%2Fwhat-rot-strawberry-proteomics-and-the-art-of-staying-fresh%2F&psig=AOvVaw0K2IvM2nm1-Cba6oqAbHK_&ust=1587568995809000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiylsPp-egCFQAAAAAdAAAAABAF'
+      'https://image.shutterstock.com/image-vector/strawberry-iconvector-illustration-flat-design-260nw-1371070088.jpg',
+    id: 1,
+    description: 'oasjfo askdjf lj woij ls;adf laskdf ;askdj ;lasdfj '
   },
   {
     name: 'strawberry',
     price: 2,
     image:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thermofisher.com%2Fblog%2Ffood%2Fwhat-rot-strawberry-proteomics-and-the-art-of-staying-fresh%2F&psig=AOvVaw0K2IvM2nm1-Cba6oqAbHK_&ust=1587568995809000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiylsPp-egCFQAAAAAdAAAAABAF'
+      ' https://image.shutterstock.com/image-vector/strawberry-iconvector-illustration-flat-design-260nw-1371070088.jpg',
+    id: 2,
+    description: 'oasjfo askdjf lj woij ls;adf laskdf ;askdj ;lasdfj '
   },
 
   {
     name: 'strawberryJam',
     price: 30,
     image:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thermofisher.com%2Fblog%2Ffood%2Fwhat-rot-strawberry-proteomics-and-the-art-of-staying-fresh%2F&psig=AOvVaw0K2IvM2nm1-Cba6oqAbHK_&ust=1587568995809000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCPiylsPp-egCFQAAAAAdAAAAABAF'
+      'https://image.shutterstock.com/image-vector/strawberry-iconvector-illustration-flat-design-260nw-1371070088.jpg',
+    id: 3,
+    description: 'oasjfo askdjf lj woij ls;adf laskdf ;askdj ;lasdfj '
   }
 ]
 
@@ -31,17 +37,18 @@ class AllProducts extends React.Component {
     return (
       <div className="all-products">
         <div className="all-products-container">
-          <div className="single-product">
-            {/* Product Image */}
-            <img src="" alt="" />
-            <Link to="/singleproduct/id">Product Name</Link>
-            <p>$20.00</p>
-            <button>Add</button>
-          </div>
-          {/* {products.map((product) => {
-            // name, price, quantity, image, add to cart button
-
-          })}  we are going to want to make the div for each item to be a link to the single item */}
+          {testArrayOfObjects.map(product => {
+            return (
+              <div className="single-product" key={product.id}>
+                <Link to={`/singleproduct/${product.id}`}>{product.name}</Link>
+                <Link to="/singleproduct/id">
+                  <img src={product.image} alt="" />
+                </Link>
+                <p>{product.price}</p>
+                <button>Add</button>
+              </div>
+            )
+          })}
         </div>
       </div>
     )
--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -4,14 +4,22 @@ import {Link} from 'react-router-dom'
 // import {Navbar} from './components'
 // import Routes from './routes'
 
+const product = {
+  name: 'strawberryShortCake',
+  price: 10,
+  image:
+    'https://image.shutterstock.com/image-vector/strawberry-iconvector-illustration-flat-design-260nw-1371070088.jpg',
+  id: 1,
+  description: 'oasjfo askdjf lj woij ls;adf laskdf ;askdj ;lasdfj '
+}
+
 class SingleProduct extends React.Component {
   render() {
     return (
-      <div className="single-product">
-        {/* Product Image */}
-        <img src="" alt="" />
-        <Link>Product Name</Link>
-        <p>$20.00</p>
+      <div className="single-product" key={product.id}>
+        <h1>{product.name}</h1>
+        <img src={product.image} alt="" />
+        <p>{product.price}</p>
         <button>Add</button>
       </div>
     )
